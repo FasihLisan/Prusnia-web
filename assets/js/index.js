c16034382033b646d4880c121c0c7eeb64357d61
ACH. FASIHUL LISAN <57977183+FasihLisan@users.noreply.github.com>
2022-10-06 09:12:41 +0700

    nex revise

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,3 +4,18 @@ $(function () {
 		$nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
 	});
 });
+
+function signin() {
+	$('.signin').show();
+	$('.signup').hide();
+}
+
+function tutup() {
+	$('.signup').hide();
+	$('.signin').hide();
+}
+
+function signup() {
+	$('.signin').hide();
+	$('.signup').show();
+}
